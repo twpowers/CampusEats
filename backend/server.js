49c860e57415e88bce92c15d3089c44d460b934e
twpowers <twpow@icloud.com>
2025-05-09 07:00:05 +0000

    added middleware

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const cors = require("cors");
+const morgan = require('morgan');
+
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-})
+
+const testRoute = require('./routes/test')
+
+app.use(express.json())
+app.use(cors());
+app.use(morgan("dev"));
+
+app.use(("/"), testRoute);
 
 app.listen(port, () => {
     console.log(`Server has started on port ${port}`)
 })
+
+
