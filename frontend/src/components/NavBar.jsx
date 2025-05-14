e17a0bccdf3adfb6468ffd455bc2baa482f35049
twpowers <twpow@icloud.com>
2025-05-14 01:28:42 +0000

    removed nofification button

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -86,6 +86,7 @@ export default function NavBar({ user, setUser }) {
                         </div>
                     </div>
                     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                        {/*}
                         <button
                             type="button"
                             className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 focus:outline-hidden"
